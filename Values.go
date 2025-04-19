56b5211e4e3e1ad9ddb4842d092f4a606b82f9c1
Om Singh <omsingh.ailearn@gmail.com>
2025-04-19 22:20:06 +0530

    2 programs done

---
+++ Values.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("go" + "lang" + "is" + "fun")
+	fmt.Println(2 + 2)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(23, 43, 55)
+}
