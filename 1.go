d09b0f06ca3cad2f99377e9423bdb576db605c54
Om Singh <omsingh.ailearn@gmail.com>
2025-04-26 18:49:32 +0530

    2.go and comments on 1.go

--- 1.go
+++ 1.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	var a int = 70
 	var str string = " Hello, World"
-	fmt.Println(str + string(a))
-	fmt.Println(str + strconv.Itoa(a))
+	fmt.Println(str + string(a))       // this converts a to UNICODE and add it
+	fmt.Println(str + strconv.Itoa(a)) // strconv.Itoa converts a which is int to string and then Concat
 }
